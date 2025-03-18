4a77c5c84e4432de9c33818839ade3652e33e02e
Jeremy <jeremywhitenect@gmail.com>
2025-03-18 08:21:53 -0300

    Adds a few more comments

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import pygame, sys
 from game import Game
 from colours import Colours
 
+# Initialize Pygame
 pygame.init()
 
+# Set up the display, this is all for text stuff
 title_font = pygame.font.Font(None, 40)
 score_surface = title_font.render("Score", True, Colours.white)
 score_rect = pygame.Rect(320, 55, 170, 60)
@@ -11,16 +13,19 @@ next_surface = title_font.render("Next", True, Colours.white)
 next_rect = pygame.Rect(320, 215, 170, 180)
 game_over_surface = title_font.render("Game Over", True, Colours.white)
 
+# Set up the window
 screen = pygame.display.set_mode((500, 620))
 pygame.display.set_caption("Tetris")
 clock = pygame.time.Clock()
-
 game = Game()
 
+# Set up the timer for the game update
 GAME_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(GAME_UPDATE, 200)
 
+# Main game loop
 while True:
+    # Check for inputs
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -41,18 +46,16 @@ while True:
         if event.type == GAME_UPDATE and game.game_over == False:
             game.move_down()
 
+    # Draw the game
     score_value_surface = title_font.render(str(game.score), True, Colours.white)
-
     screen.fill(Colours.dark_blue)
     screen.blit(score_surface, (365, 20, 50, 50))
     screen.blit(next_surface, (375, 180, 50, 50))
-
     if game.game_over == True:
         screen.blit(game_over_surface, (320, 450, 50, 50))
     pygame.draw.rect(screen, Colours.light_blue, score_rect, 0, 10)
     pygame.draw.rect(screen, Colours.light_blue, next_rect, 0, 10)
     screen.blit(score_value_surface, score_value_surface.get_rect(centerx = score_rect.centerx, centery = score_rect.centery))
     game.draw(screen)
-
     pygame.display.update()
     clock.tick(60)
